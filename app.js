acae7fa8474b728889018ded91e56332caf438c1
unknown <[200~123026227+ichiyume@users.noreply.github.com>
2023-02-24 22:39:43 +0900

    課題提出2回目

---
+++ app.js
@@ -0,0 +1,128 @@
+
+let x = 0;
+const now ='今';
+const kai ='回ループしました。';
+const yb ='4で割れる数字です。';
+const endend='回カウントが終了しました。';
+
+/*--!1桁--*/
+while(x<3){
+  x++;
+  console.log(x);
+  }
+ if(x=4){
+  console.log(yb+x)
+ }
+ while(x<7){
+  x++;
+  console.log(x);
+  }
+if(x=8){
+  console.log(yb+x)
+  }
+  while(x<9){
+    x++;
+    console.log(x);
+    }
+
+if(x=10){
+  console.log(now + x + kai)
+}
+
+/*--10の桁--*/
+while(x<11){
+  x++;
+  console.log(x)
+}
+
+if(x=12){
+  console.log(yb+x)
+ }
+ while(x<15){
+  x++;
+  console.log(x);
+  }
+if(x=16){
+  console.log(yb+x)
+  }
+  while(x<19){
+    x++;
+    console.log(x);
+    }
+if(x=20){
+  console.log(now + x + kai+yb)
+}
+
+/*--20の桁--*/
+while(x<23){
+  x++;
+  console.log(x)
+}
+if(x=24){
+  console.log(yb+x)
+ }
+ while(x<27){
+  x++;
+  console.log(x);
+  }
+if(x=28){
+  console.log(yb+x)
+  }
+  while(x<29){
+    x++;
+    console.log(x);
+    }
+if(x=30){
+  console.log(now + x + kai)
+}
+/*--30の桁--*/
+while(x<31){
+  x++;
+  console.log(x)
+}
+if(x=32){
+  console.log(yb+x)
+ }
+ while(x<35){
+  x++;
+  console.log(x);
+  }
+if(x=36){
+    console.log(yb+x)
+  }
+while(x<39){
+ x++;
+ console.log(x);
+  }
+if(x=40){
+  console.log(now + x + kai+ yb )
+}
+/*--40の桁--*/
+while(x<43){
+  x++;
+  console.log(x)
+}
+if(x=44){
+  console.log(yb+x)
+ }
+ while(x<47){
+  x++;
+  console.log(x);
+   }
+   while(x<47){
+    x++;
+    console.log(x)
+  }
+ if(x=48){
+    console.log(yb+x)
+   } 
+  while(x<49){
+    x++;
+    console.log(x)
+  }
+
+if(x=50){
+  console.log(x+ endend)
+  alert('50回カウントが終わりました。');
+  
+}
